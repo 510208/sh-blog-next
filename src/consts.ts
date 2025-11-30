c1115ee42a7921b62b5884a0799f4d1959cc27ad
SamHacker <xux510208@gmail.com>
2025-11-30 21:14:59 +0800

    ✨ feat: 新增作者卡片組件並更新網站標題與描述

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "SamHacker Blog";
+export const SITE_DESCRIPTION =
+  "我是一個熱愛分享技術的部落客，專注於 Minecraft 開服、網站建設、開源軟體等領域。";
