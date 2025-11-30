26503b4a69c2aa8c478f285adb6b19e2484589ac
SamHacker <xux510208@gmail.com>
2025-11-30 21:05:49 +0800

    🔧 feat: add configuration file and update TypeScript settings
    - Created a new configuration file `shblog.config.ts` for navigation links.
    - Updated `tsconfig.json` to include JSX support for React and configured path aliases.

---
+++ shblog.config.ts
@@ -0,0 +1,11 @@
+const config = {
+  navBar: {
+    links: [
+      { title: "Home", href: "/" },
+      { title: "About", href: "/about" },
+      { title: "Contact", href: "/contact" },
+    ],
+  },
+};
+
+export default config;
