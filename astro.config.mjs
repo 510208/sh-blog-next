e276592cebf6881b8026d6ec47cd1aa19ed6771d
SamHacker <xux510208@gmail.com>
2025-12-07 21:27:26 +0800

    ✨ feat: 更新 astro.config.mjs，新增多個 remark 和 rehype 插件以增強 Markdown 處理功能

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,23 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import remarkDirective from "remark-directive"; /* Handle directives */
+import remarkGithubAdmonitionsToDirectives from "remark-github-admonitions-to-directives";
+import remarkMath from "remark-math";
+import remarkSectionize from "remark-sectionize";
+import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
+import remarkSpoiler from "./src/plugins/remark-spoiler.js";
+
+// @ts-ignore
+import rehypeCodeBlock from "./src/plugins/rehype-code-block.mjs";
+// @ts-ignore
+import shikiCodeMetadata from "./src/plugins/shiki-code-metadata.mjs";
+import rehypeCodeTitles from "rehype-code-titles";
+// @ts-ignore
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
+import rehypeKatex from "rehype-katex";
+
 import react from "@astrojs/react";
 
 // https://astro.build/config
@@ -16,5 +33,58 @@ export default defineConfig({
   },
   experimental: {
     svgo: true,
+  },
+  markdown: {
+    shikiConfig: {
+      // 添加 Shiki transformer 來處理代碼區塊的 metadata
+      transformers: [shikiCodeMetadata()],
+    },
+    remarkPlugins: [
+      remarkMath,
+      remarkReadingTime,
+      remarkGithubAdmonitionsToDirectives,
+      remarkDirective,
+      remarkSectionize,
+      remarkSpoiler,
+    ],
+    rehypePlugins: [
+      rehypeKatex,
+      rehypeCodeTitles,
+      // rehype-code-block 必須在其他處理代碼的插件之後
+      rehypeCodeBlock,
+      // [
+      //   rehypeComponents,
+      //   {
+      //     components: {
+      //       // 例如你要讓 <Alert> 用自訂 React 組件呈現
+      //       alert: require("./src/components/Alert.jsx").default,
+      //     },
+      //   },
+      // ],
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "prepend",
+          properties: {
+            className: ["anchor"],
+          },
+          content: {
+            type: "element",
+            tagName: "span",
+            properties: {
+              className: ["anchor-icon"],
+              "data-pagefind-ignore": true,
+            },
+            children: [
+              {
+                type: "text",
+                value: "#",
+              },
+            ],
+          },
+        },
+      ],
+    ],
   },
 });
