b14ad441731d09853c8616e7aa7b11ef09e76c77
SamHacker <xux510208@gmail.com>
2025-12-07 08:53:15 +0800

    🎨 feat: 更新 shblog.config.ts 以包含導航欄圖示，並在 consts.ts 中引用配置

--- shblog.config.ts
+++ shblog.config.ts
@@ -1,9 +1,26 @@
+import { Home, Info, Phone } from "lucide-react";
+
 const config = {
+  title: "SamHacker Blog",
+  description:
+    "我是一個熱愛分享技術的部落客，專注於 Minecraft 開服、網站建設、開源軟體等領域。",
   navBar: {
     links: [
-      { title: "Home", href: "/" },
-      { title: "About", href: "/about" },
-      { title: "Contact", href: "/contact" },
+      {
+        title: "Home",
+        href: "/",
+        icon: Home,
+      },
+      {
+        title: "About",
+        href: "/about",
+        icon: Info,
+      },
+      {
+        title: "Contact",
+        href: "/contact",
+        icon: Phone,
+      },
     ],
   },
 };
--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
+import config from "../shblog.config";
 
-export const SITE_TITLE = "SamHacker Blog";
-export const SITE_DESCRIPTION =
-  "我是一個熱愛分享技術的部落客，專注於 Minecraft 開服、網站建設、開源軟體等領域。";
+export const SITE_TITLE = config.title;
+export const SITE_DESCRIPTION = config.description;
