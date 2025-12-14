b00f2411a8ae3a061d3e78b1422b1ff80855fb51
SamHacker <xux510208@gmail.com>
2025-12-14 19:12:47 +0800

    ✨ feat: 重構 StickyButtons 和 ToComment 元件，新增留言按鈕，並調整滾動行為

---
+++ src/components/global/ToComment.tsx
@@ -0,0 +1,36 @@
+import { MessageCircle } from "lucide-react";
+import { Button } from "../ui/button";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+
+export const ToCommentButton = ({ className }: { className?: string }) => {
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <>
+          {/* 電腦版回頂端按鈕 */}
+          <Button
+            id="to-comment-button"
+            aria-label="Jump to comments"
+            variant={"ghost"}
+            className={`hidden lg:block backdrop-blur-md ${className}`}
+          >
+            <MessageCircle className="w-5 h-5 text-white lg:rotate-90" />
+          </Button>
+
+          {/* 手機版回頂端按鈕 */}
+          <Button
+            id="to-comment-button-mobile"
+            aria-label="Jump to comments"
+            variant={"outline"}
+            className={`lg:hidden block backdrop-blur-md ${className}`}
+          >
+            <MessageCircle className="w-5 h-5 text-white lg:rotate-90" />
+          </Button>
+        </>
+      </TooltipTrigger>
+      <TooltipContent side="left">
+        <p>前往留言區</p>
+      </TooltipContent>
+    </Tooltip>
+  );
+};
