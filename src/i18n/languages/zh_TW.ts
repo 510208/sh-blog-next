53186f15b5a325e7ea1f3fea4735dec5117e71a6
SamHacker <xux510208@gmail.com>
2026-01-24 22:28:06 +0800

    🌐 i18n: 新增英文及繁體中文翻譯檔案

---
+++ src/i18n/languages/zh_TW.ts
@@ -0,0 +1,28 @@
+import Key from "../i18nKey";
+import type { Translation } from "../translation";
+
+export const zh_TW: Translation = {
+  [Key.home_recent_posts]: "最近更新",
+
+  [Key.posts_timeline_year_post_count_single]: "篇文章",
+  [Key.posts_timeline_year_post_count_plural]: "篇文章",
+
+  [Key.post_released]: "文章發布時間",
+  [Key.post_last_edited]: "更新時間",
+  [Key.post_author]: "作者",
+  [Key.post_tags]: "標籤",
+  [Key.post_toc_title]: "目錄",
+
+  [Key.notfound_title]: "要找的東西不在這！",
+  [Key.notfound_description]: "找不到目標的頁面，請查明後再開...",
+  [Key.notfound_bsod_title_line_1]: "您的裝置發生問題，但我們不可能重新啟動。",
+  [Key.notfound_bsod_title_line_2]:
+    "我們根本不會收集任何錯誤資訊，所以你也別期待會為你重新啟動。",
+  [Key.notfound_finished_percent_description]: "已完成",
+  [Key.notfound_bsod_description_1]: "上網公審站長時，請提供此資訊：",
+  [Key.notfound_bsod_description_2]: "停止代碼：THERE_IS_NOTHING",
+  [Key.notfound_bsod_failed_item]: "失敗的項目：",
+  [Key.notfound_press_any_key_1]: "按下",
+  [Key.notfound_press_any_key_2]: "任意鍵",
+  [Key.notfound_press_any_key_3]: "以返回首頁...",
+};
